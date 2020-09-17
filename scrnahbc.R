21955cac3ad664c0d00e732a9dd3d1deec484dda
Artur Manukyan <artur-man@hotmail.com>
2020-09-17 14:10:17 -0400

    Update scrnahbc.R

--- scrnahbc.R
+++ scrnahbc.R
@@ -1,10 +1,12 @@
 install.packages("BiocManager", repos='http://cran.us.r-project.org')
 library(BiocManager)
-install(c('Seurat',
-          "SingleCellExperiment",
-          "tidyverse",
-          "dplyr",
-          "Matrix",
-          "scales",
-          "RCurl")
-)
+install("curl")
+install("httr")
+install('Seurat')
+install('SingleCellExperiment')
+install('xml2')
+install('tidyverse')
+install("dplyr")
+install('Matrix')
+install('scales')
+install('RCurl')
