4a69e9ae25cf77c69270b5b33f37ca387dfa2284
Artur Manukyan <artur-man@hotmail.com>
2020-09-22 15:46:14 -0400

    Update scrnahbc.R

--- scrnahbc.R
+++ scrnahbc.R
@@ -1,4 +1,5 @@
 install.packages("BiocManager", repos='http://cran.us.r-project.org')
+install.packages("rmarkdown", repos='http://cran.us.r-project.org')
 library(BiocManager)
 install("curl")
 install("httr")
